b1fc58fe5b8b75eeab7fda0db18f2e7b79cdf216
luhcapellini <capellini.luana@gmail.com>
2021-03-24 08:32:57 -0300

    adicao teste de remocao

--- src/test/java/com/luhcapellini/TasksTest.java
+++ src/test/java/com/luhcapellini/TasksTest.java
@@ -115,4 +115,27 @@ public class TasksTest {
             driver.quit();
         }
     }
+    @Test
+    public void deveRemoverTarefaComSucesso() {
+        WebDriver driver = acessarAplicacao();
+        try {
+            //inserir tarefa
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2021");
+            driver.findElement(By.id("saveButton")).click();
+            String message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+
+            // Remover a tarefa
+            driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+            message = driver.findElement(By.id("message")).getText();
+            Assert.assertEquals("Success!", message);
+
+        } finally {
+            //fechar o browser
+            driver.quit();
+        }
+    }
 }
+
